8693790bc2d220bcc83ac8b87c21b0e52ff49a4b
betulnesibe <80793176+betulnesibe@users.noreply.github.com>
2024-10-16 21:16:54 +0300

    icp2 is added

---
+++ icp2/Assets/Scripts/BallBooster.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBooster : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 direction = new Vector3(-5, 0, 0);
+    private Vector3 startPos;
+
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(direction);
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "brickWall")
+        {
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+    }
+}
