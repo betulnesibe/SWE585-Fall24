72edf27e04fc2d306f63929855dd661b2bbf2eb2
betulnesibe <80793176+betulnesibe@users.noreply.github.com>
2024-10-20 09:50:41 +0300

    2.6 is completed.

--- hw1/Assets/Scripts/AddForceScript.cs
+++ hw1/Assets/Scripts/AddForceScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class AddForceScript : MonoBehaviour
 {
+
+    public Rigidbody rb; // to make it publicly accessible
+    public float force = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (rb== null) // if not added from ui
+        {
+            rb = GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(new Vector3(1, 0, 0) * force);
     }
 }
